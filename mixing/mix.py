5c84ef51158f30d9e8841968fd718c7c602a5edf
KashifIV <kashif.h77@gmail.com>
2022-08-23 13:03:32 -0400

    check for sticky keys

--- mixing/mix.py
+++ mixing/mix.py
@@ -2,6 +2,7 @@ from mixing.MGS.song import Song
 from mido import MidiFile
 from mixing.assign_program import get_program
 import os 
+import numpy as np 
 import subprocess 
 
 
@@ -13,12 +14,42 @@ def to_wav(path, output_path):
     'fluidsynth', sound_file, '-F','{}.wav'.format(output_path.split('.')[0]), path
   ]
   proc = subprocess.run(wav_command)
-  return proc.stdout == 0 
+  return proc.stdout == 0
+
+def is_sticky(file_path): 
+  max_length = 24*5
+  song = Song(MidiFile(file_path))
+  pr = song.to_pianoroll()
+
+  track_index = None
+  current_streak = 0
+  for x in range(pr.tracks[0].pianoroll.shape[0]):
+    notes = [] 
+    for track in pr.tracks: 
+      notes.append(np.sum(track.pianoroll[x]))
+    notes = np.array(notes)
+
+    index = np.argmin(notes)
+    if notes[index] == np.sum(notes): 
+      if track_index == index:
+        current_streak +=1
+        if current_streak >= max_length: 
+          return True 
+      else: 
+        track_index = index 
+        current_streak = 1
+    else: 
+      track_index = None 
+      current_streak = 0
+  return False 
+
+
 
 def mix(file_path, output_dir): 
   song = Song(MidiFile(file_path))
   midi = get_program(song)
   path = '{}/mixed_music/'.format(dir_path) + file_path.split('/')[-1]
   midi.write(path)
-  to_wav(path, output_dir + file_path.split('/')[-1])
+  if not is_sticky(path): 
+    to_wav(path, output_dir + file_path.split('/')[-1])
   os.remove(path)
